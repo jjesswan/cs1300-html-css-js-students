53bc11379fd7342846071b9a9ed1c1f1c488f3dd
jjesswan <jessica_wan@brown.edu>
2022-09-10 17:39:54 -0400

    Finished

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const apiRequest = async () => {
    */
 
   // TODO fill in your own port number 
-  const PORT_NUMBER = "";
+  const PORT_NUMBER = "8010";
 
   const baseUrl = `http://localhost:${PORT_NUMBER}/proxy/api/`
 
@@ -40,14 +40,33 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15", 
 
+  const highSugarArray = fruitsArray.filter((item) => {
+    return item.nutritions.sugar > 15;
+  })
+
+  const itemArray = highSugarArray.map((item) => {
+    return item.name;
+  })
+
   // TODO: Create a new HTML element to display your data 
 
+  const fruitElement = document.createElement('div');
+  fruitElement.innerHTML = "Fruits with sugar over 15 " + itemArray;
+
+
   // TODO: Append your new element to the page
+  const existingElement = document.getElementById('cs1300-gallery');
+  //fruitElement.forEach(addToExisting())
+  existingElement.append(fruitElement);
+
+  // function addToExisting(item){
+  //   existingElement.append(item);
+  // }
 
 }
 
